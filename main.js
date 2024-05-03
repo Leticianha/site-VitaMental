68c0587b661490612c8f10d370272f9e1aae1bf5
Leticia Cristina <letici4cr1s@gmail.com>
2024-05-03 01:50:20 -0300

    finish

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 function openMenu() {
-  var popup = document.getElementById("popup");
-  if (popup.style.display === "block") {
-      popup.style.display = "none";
-  } else {
-      popup.style.display = "block";
+    var popup = document.getElementById("popup");
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
+    }
   }
-}
+  
+  function closeMenu() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
 
-function closeMenu() {
-  var popup = document.getElementById("popup");
-  popup.style.display = "none";
-}
+  
